357674bbd64d38ac879c1f80fae8cef0b7361117
guilucas2000 <guilucas@gmail.com>
2015-10-22 09:37:08 -0200

    Corsera Assignment 2: Lexical Scoping
    The Assignment 2: Lexical Scoping of week3. R programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  Coursera task number2
+## It calculate the inverse matrix once, for a cache object.
 
-## Write a short comment describing this function
+## The program will make a cacheMatrix object for an inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+cachedInverse <- NULL
+set <- function(m){
+  X<<- m
+  cachedInverse <<- NULL
+}
+get<- function() x
+setInverse <- function(inverse) cachedInverse <<- inverse
+getInverse <- function() cachedInverse
+list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## this function works by returning a object that is the inverse of cacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invFun <- x$getInverse()
+  if(!is.null(inFun)){
+    message("get cache data")
+    return(invFun)
+  }
+  data <-x$get()
+  invFun <- solve(data, ...)
+  x$setInverse(invFun)
+  invFun
 }
